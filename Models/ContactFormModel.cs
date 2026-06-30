97e5c41e94bd526c1c8a7a4b8f8cd50b23e1f42f
Archimistry17 <archimistry2k@gmail.com>
2026-06-30 11:38:13 +0530

    Add files via upload

---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AerodyneCompressors.Models
+{
+    public class ContactFormModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone]
+        public string Phone { get; set; }
+
+        public string Company { get; set; }
+
+        [Required(ErrorMessage = "Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter your question/message")]
+        [Display(Name = "Question")]
+        public string Question { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AerodyneCompressors.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddResponseCompression(options => {
+    options.EnableForHttps = true;
+});
+
+// 🚀 ENFORCE CLEAN LOWERCASE URLS FOR INDUSTRIAL SEARCH ENGINES
+builder.Services.Configure<RouteOptions>(options => {
+    options.LowercaseUrls = true;
+    options.AppendTrailingSlash = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseResponseCompression();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
